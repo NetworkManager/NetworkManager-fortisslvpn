386b253bf6bd31315f5e645f519d6870810f6f1b
Thomas Haller <thaller@redhat.com>
2016-09-16 16:32:37 +0200

    src: make valid_properties array const

--- src/nm-fortisslvpn-service.c
+++ src/nm-fortisslvpn-service.c
@@ -78,25 +78,25 @@ typedef struct {
 typedef struct {
 	const char *name;
 	GType type;
-	gboolean required;
+	bool required:1;
 } ValidProperty;
 
 /*****************************************************************************/
 
-static ValidProperty valid_properties[] = {
+static const ValidProperty valid_properties[] = {
 	{ NM_FORTISSLVPN_KEY_GATEWAY,           G_TYPE_STRING, TRUE },
 	{ NM_FORTISSLVPN_KEY_USER,              G_TYPE_STRING, TRUE },
 	{ NM_FORTISSLVPN_KEY_CA,                G_TYPE_STRING, FALSE },
 	{ NM_FORTISSLVPN_KEY_TRUSTED_CERT,      G_TYPE_STRING, FALSE },
 	{ NM_FORTISSLVPN_KEY_CERT,              G_TYPE_STRING, FALSE },
 	{ NM_FORTISSLVPN_KEY_KEY,               G_TYPE_STRING, FALSE },
-	{ NM_FORTISSLVPN_KEY_PASSWORD"-flags",  G_TYPE_UINT, FALSE },
-	{ NULL,                                 G_TYPE_NONE, FALSE }
+	{ NM_FORTISSLVPN_KEY_PASSWORD"-flags",  G_TYPE_UINT,   FALSE },
+	{ NULL }
 };
 
-static ValidProperty valid_secrets[] = {
+static const ValidProperty valid_secrets[] = {
 	{ NM_FORTISSLVPN_KEY_PASSWORD,          G_TYPE_STRING, TRUE },
-	{ NULL,                                 G_TYPE_NONE,   FALSE }
+	{ NULL }
 };
 
 /*****************************************************************************/
@@ -161,7 +161,7 @@ validate_ca (const char *ca)
 }
 
 typedef struct ValidateInfo {
-	ValidProperty *table;
+	const ValidProperty *table;
 	GError **error;
 	gboolean have_items;
 } ValidateInfo;
